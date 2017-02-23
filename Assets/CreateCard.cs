25b55f0c7d6a6e0a3ca09c970de025ce898893c3
AkatokiKeisuke <is0291vi@ed.ritsumei.ac.jp>
2017-02-23 19:14:47 +0900

    わーい！カード生成時に物理演算使うのやめたよー！

---
+++ Assets/CreateCard.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Define{
+	public const int SHEETS=53;
+
+}
+public class CreateCard : MonoBehaviour {
+
+	public GameObject[] card=new GameObject[Define.SHEETS];
+	public GameObject[] Deck=new GameObject[Define.SHEETS];
+	//int cardCount=0;
+	private GameObject trump;
+	private float SetCard=0.0f;
+	private Vector3 DeckPos;
+
+
+ 	void Shuffle (GameObject[] deck) {
+        for (int i = 0; i < deck.Length; i++) {
+            GameObject temp = deck[i];
+            int randomIndex = Random.Range(0, deck.Length);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		DeckPos=this.transform.position;
+		for(int i=0;i<Deck.Length;i++){
+			trump=Instantiate(card[i]) as GameObject;
+			Deck[i]=trump;	
+		}
+		Shuffle(Deck);
+
+		for(int i=0;i<Deck.Length;i++){
+			Deck[i].transform.position=new Vector3(DeckPos.x,SetCard,DeckPos.z);
+			SetCard+=0.01f;
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/MouseToObj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseToObj : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Camera.main.ScreenToWorldPoint(pos);
+        Vector3 pos = GetPos();
+        Vector3 position = Camera.main.ScreenToWorldPoint(pos); ;
+		transform.position=new Vector3(position.x,0.3f,position.z);
+    }
+
+    private static Vector3 GetPos()
+    {
+        return Input.mousePosition;
+    }
+}
